295f96bbecd161bbb99e1b73bea3d649c76c79c7
AxelGarciaR <20220127@ricaldone.edu.sv>
2025-04-13 23:33:40 -0600

    Creación de interfaz de la opción creación de figuras

--- Formularios/Generacion de figuras.Designer.cs
+++ Formularios/Generacion de figuras.Designer.cs
@@ -28,23 +28,251 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GeneracionFiguras));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnCrearCorazon = new System.Windows.Forms.Button();
+            this.coldialogColorMatriz = new System.Windows.Forms.ColorDialog();
+            this.tckbarBrillo = new System.Windows.Forms.TrackBar();
+            this.btnCrearTriangulo = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btnCrearCuadrado = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.btnCrearGato = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.btnCrearHi = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.btnCrearNuevaFig = new System.Windows.Forms.Button();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.btnCambiarColor = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tckbarBrillo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(34, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(117, 116);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnCrearCorazon
+            // 
+            this.btnCrearCorazon.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnCrearCorazon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrearCorazon.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCrearCorazon.Location = new System.Drawing.Point(34, 159);
+            this.btnCrearCorazon.Name = "btnCrearCorazon";
+            this.btnCrearCorazon.Size = new System.Drawing.Size(117, 28);
+            this.btnCrearCorazon.TabIndex = 6;
+            this.btnCrearCorazon.Text = "Crear corazon";
+            this.btnCrearCorazon.UseVisualStyleBackColor = false;
+            // 
+            // tckbarBrillo
+            // 
+            this.tckbarBrillo.Location = new System.Drawing.Point(193, 441);
+            this.tckbarBrillo.Name = "tckbarBrillo";
+            this.tckbarBrillo.Size = new System.Drawing.Size(281, 45);
+            this.tckbarBrillo.TabIndex = 25;
+            this.tckbarBrillo.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // btnCrearTriangulo
+            // 
+            this.btnCrearTriangulo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnCrearTriangulo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrearTriangulo.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCrearTriangulo.Location = new System.Drawing.Point(193, 159);
+            this.btnCrearTriangulo.Name = "btnCrearTriangulo";
+            this.btnCrearTriangulo.Size = new System.Drawing.Size(117, 28);
+            this.btnCrearTriangulo.TabIndex = 14;
+            this.btnCrearTriangulo.Text = "Crear triangulo";
+            this.btnCrearTriangulo.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(193, 28);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(117, 116);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 13;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btnCrearCuadrado
+            // 
+            this.btnCrearCuadrado.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnCrearCuadrado.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrearCuadrado.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCrearCuadrado.Location = new System.Drawing.Point(357, 159);
+            this.btnCrearCuadrado.Name = "btnCrearCuadrado";
+            this.btnCrearCuadrado.Size = new System.Drawing.Size(117, 28);
+            this.btnCrearCuadrado.TabIndex = 16;
+            this.btnCrearCuadrado.Text = "Crear cuadrado";
+            this.btnCrearCuadrado.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(357, 28);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(117, 116);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            // 
+            // btnCrearGato
+            // 
+            this.btnCrearGato.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnCrearGato.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrearGato.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCrearGato.Location = new System.Drawing.Point(34, 346);
+            this.btnCrearGato.Name = "btnCrearGato";
+            this.btnCrearGato.Size = new System.Drawing.Size(117, 28);
+            this.btnCrearGato.TabIndex = 18;
+            this.btnCrearGato.Text = "Crear gato";
+            this.btnCrearGato.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(34, 215);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(117, 116);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            // 
+            // btnCrearHi
+            // 
+            this.btnCrearHi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnCrearHi.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrearHi.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCrearHi.Location = new System.Drawing.Point(193, 346);
+            this.btnCrearHi.Name = "btnCrearHi";
+            this.btnCrearHi.Size = new System.Drawing.Size(117, 28);
+            this.btnCrearHi.TabIndex = 20;
+            this.btnCrearHi.Text = "Crear hi";
+            this.btnCrearHi.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(193, 215);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(117, 116);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 19;
+            this.pictureBox5.TabStop = false;
+            // 
+            // btnCrearNuevaFig
+            // 
+            this.btnCrearNuevaFig.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnCrearNuevaFig.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCrearNuevaFig.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCrearNuevaFig.Location = new System.Drawing.Point(357, 346);
+            this.btnCrearNuevaFig.Name = "btnCrearNuevaFig";
+            this.btnCrearNuevaFig.Size = new System.Drawing.Size(117, 28);
+            this.btnCrearNuevaFig.TabIndex = 22;
+            this.btnCrearNuevaFig.Text = "Nueva figura";
+            this.btnCrearNuevaFig.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox6.Image")));
+            this.pictureBox6.Location = new System.Drawing.Point(357, 215);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(117, 116);
+            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox6.TabIndex = 21;
+            this.pictureBox6.TabStop = false;
+            // 
+            // btnCambiarColor
+            // 
+            this.btnCambiarColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnCambiarColor.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCambiarColor.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnCambiarColor.Location = new System.Drawing.Point(34, 441);
+            this.btnCambiarColor.Name = "btnCambiarColor";
+            this.btnCambiarColor.Size = new System.Drawing.Size(117, 45);
+            this.btnCambiarColor.TabIndex = 23;
+            this.btnCambiarColor.Text = "Crear gato";
+            this.btnCambiarColor.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(32, 396);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(441, 20);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "________________________________________________";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // GeneracionFiguras
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(56)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(523, 512);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCambiarColor);
+            this.Controls.Add(this.btnCrearNuevaFig);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.btnCrearHi);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.btnCrearGato);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.btnCrearCuadrado);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.btnCrearTriangulo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.tckbarBrillo);
+            this.Controls.Add(this.btnCrearCorazon);
+            this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "GeneracionFiguras";
             this.Text = "Generacion de figuras";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tckbarBrillo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnCrearCorazon;
+        private System.Windows.Forms.ColorDialog coldialogColorMatriz;
+        private System.Windows.Forms.TrackBar tckbarBrillo;
+        private System.Windows.Forms.Button btnCrearTriangulo;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnCrearCuadrado;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button btnCrearGato;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button btnCrearHi;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button btnCrearNuevaFig;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Button btnCambiarColor;
+        private System.Windows.Forms.Label label1;
     }
 }
