d5b1d91a885a7a37b2cdcb767c738cd14e64fda7
Santibooy <santiago.vlopez11@gmail.com>
2025-04-30 11:53:24 -0600

    Add files via upload

--- Formularios/Crear nueva figura.cs
+++ Formularios/Crear nueva figura.cs
@@ -1,20 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace PingPong_Generacion_de_figuras_Grupo3
+namespace MatrizLED
 {
     public partial class CrearNuevaFigura : Form
     {
+        CheckBox[,] matriz;
+
         public CrearNuevaFigura()
         {
             InitializeComponent();
+            CrearCheckBoxes();
+            DibujarXD();
+        }
+
+        private void CrearCheckBoxes()
+        {
+            int tamaño = 30;
+            matriz = new CheckBox[8, 8];
+
+            for (int fila = 0; fila < 8; fila++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    CheckBox chk = new CheckBox();
+                    chk.Width = chk.Height = tamaño;
+                    chk.Location = new Point(20 + col * tamaño, 20 + fila * tamaño);
+                    chk.Appearance = Appearance.Button;
+                    chk.FlatStyle = FlatStyle.Flat;
+                    this.Controls.Add(chk);
+                    matriz[fila, col] = chk;
+                }
+            }
+        }
+
+        private void DibujarXD()
+        {
+            foreach (var chk in matriz) chk.Checked = false;
+
+            matriz[1, 1].Checked = true;
+            matriz[2, 2].Checked = true;
+            matriz[3, 3].Checked = true;
+            matriz[4, 4].Checked = true;
+            matriz[5, 5].Checked = true;
+            matriz[1, 5].Checked = true;
+            matriz[2, 4].Checked = true;
+            matriz[3, 3].Checked = true;
+            matriz[4, 2].Checked = true;
+            matriz[5, 1].Checked = true;
+
+            matriz[1, 7].Checked = true;
+            matriz[2, 7].Checked = true;
+            matriz[3, 7].Checked = true;
+            matriz[4, 7].Checked = true;
+            matriz[5, 7].Checked = true;
+            matriz[1, 6].Checked = true;
+            matriz[5, 6].Checked = true;
+            matriz[2, 5].Checked = true;
+            matriz[3, 5].Checked = true;
+            matriz[4, 5].Checked = true;
         }
     }
 }
---
+++ Formularios/crear figura.Designer.cs
@@ -0,0 +1,17 @@
+namespace MatrizLED
+{
+    partial class CrearNuevaFigura
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // CrearNuevaFigura
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 400);
+            this.Name = "CrearNuevaFigura";
+            this.Text = "Matriz LED - XD";
+            this.ResumeLayout(false);
+        }
+    }
+}
