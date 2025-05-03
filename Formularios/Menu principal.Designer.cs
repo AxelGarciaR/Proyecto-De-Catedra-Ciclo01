b37846668c0293c07f816e255953a49f9062073c
eduardzsosa72 <eduardzsosa72@gmail.com>
2025-05-03 17:08:00 -0600

    solo esto avance

--- Formularios/Menu principal.Designer.cs
+++ Formularios/Menu principal.Designer.cs
@@ -20,7 +20,7 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
+      #region Código generado por el Diseñador de Windows Forms
 
         /// <summary>
         /// Método necesario para admitir el Diseñador. No se puede modificar
@@ -59,11 +59,11 @@
             // lbGeneracion
             // 
             this.lbGeneracion.AutoSize = true;
-            this.lbGeneracion.Font = new System.Drawing.Font("Monocraft", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbGeneracion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbGeneracion.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.lbGeneracion.Location = new System.Drawing.Point(200, 209);
             this.lbGeneracion.Name = "lbGeneracion";
-            this.lbGeneracion.Size = new System.Drawing.Size(241, 22);
+            this.lbGeneracion.Size = new System.Drawing.Size(166, 20);
             this.lbGeneracion.TabIndex = 1;
             this.lbGeneracion.Text = "Generacion de figuras";
             this.lbGeneracion.MouseLeave += new System.EventHandler(this.lbGeneracion_MouseLeave);
@@ -72,11 +72,11 @@
             // lbJugarPingPong
             // 
             this.lbJugarPingPong.AutoSize = true;
-            this.lbJugarPingPong.Font = new System.Drawing.Font("Monocraft", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbJugarPingPong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbJugarPingPong.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.lbJugarPingPong.Location = new System.Drawing.Point(231, 277);
             this.lbJugarPingPong.Name = "lbJugarPingPong";
-            this.lbJugarPingPong.Size = new System.Drawing.Size(175, 22);
+            this.lbJugarPingPong.Size = new System.Drawing.Size(125, 20);
             this.lbJugarPingPong.TabIndex = 2;
             this.lbJugarPingPong.Text = "Jugar Ping Pong";
             this.lbJugarPingPong.MouseLeave += new System.EventHandler(this.lbJugarPingPong_MouseLeave);
@@ -85,11 +85,11 @@
             // lbCerrarPrograma
             // 
             this.lbCerrarPrograma.AutoSize = true;
-            this.lbCerrarPrograma.Font = new System.Drawing.Font("Monocraft", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCerrarPrograma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbCerrarPrograma.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.lbCerrarPrograma.Location = new System.Drawing.Point(231, 347);
             this.lbCerrarPrograma.Name = "lbCerrarPrograma";
-            this.lbCerrarPrograma.Size = new System.Drawing.Size(175, 22);
+            this.lbCerrarPrograma.Size = new System.Drawing.Size(126, 20);
             this.lbCerrarPrograma.TabIndex = 3;
             this.lbCerrarPrograma.Text = "Cerrar Programa";
             this.lbCerrarPrograma.MouseLeave += new System.EventHandler(this.lbCerrarPrograma_MouseLeave);
@@ -132,11 +132,11 @@
             // lbAyuda
             // 
             this.lbAyuda.AutoSize = true;
-            this.lbAyuda.Font = new System.Drawing.Font("Monocraft", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbAyuda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbAyuda.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.lbAyuda.Location = new System.Drawing.Point(279, 415);
             this.lbAyuda.Name = "lbAyuda";
-            this.lbAyuda.Size = new System.Drawing.Size(65, 22);
+            this.lbAyuda.Size = new System.Drawing.Size(54, 20);
             this.lbAyuda.TabIndex = 7;
             this.lbAyuda.Text = "Ayuda";
             this.lbAyuda.MouseLeave += new System.EventHandler(this.lbAyuda_MouseLeave);
@@ -174,6 +174,7 @@
             this.Name = "MenuPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Menu principal";
+            this.Load += new System.EventHandler(this.MenuPrincipal_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbSelect1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbSelect2)).EndInit();
@@ -185,7 +186,182 @@
         }
 
         #endregion
-
+        private void initializeCompnent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuPrincipal));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lbGeneracion = new System.Windows.Forms.Label();
+            this.lbJugarPingPong = new System.Windows.Forms.Label();
+            this.lbCerrarPrograma = new System.Windows.Forms.Label();
+            this.pbSelect1 = new System.Windows.Forms.PictureBox();
+            this.pbSelect2 = new System.Windows.Forms.PictureBox();
+            this.pbSelect3 = new System.Windows.Forms.PictureBox();
+            this.lbAyuda = new System.Windows.Forms.Label();
+            this.pbSelect4 = new System.Windows.Forms.PictureBox();
+            // Declaración de lblStart
+            this.lblStart = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(182, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(277, 142);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lbGeneracion
+            // 
+            this.lbGeneracion.AutoSize = true;
+            this.lbGeneracion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbGeneracion.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbGeneracion.Location = new System.Drawing.Point(200, 209);
+            this.lbGeneracion.Name = "lbGeneracion";
+            this.lbGeneracion.Size = new System.Drawing.Size(166, 20);
+            this.lbGeneracion.TabIndex = 1;
+            this.lbGeneracion.Text = "Generacion de figuras";
+            this.lbGeneracion.MouseLeave += new System.EventHandler(this.lbGeneracion_MouseLeave);
+            this.lbGeneracion.MouseHover += new System.EventHandler(this.lbGeneracion_MouseHover);
+            // 
+            // lbJugarPingPong
+            // 
+            this.lbJugarPingPong.AutoSize = true;
+            this.lbJugarPingPong.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbJugarPingPong.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbJugarPingPong.Location = new System.Drawing.Point(231, 277);
+            this.lbJugarPingPong.Name = "lbJugarPingPong";
+            this.lbJugarPingPong.Size = new System.Drawing.Size(125, 20);
+            this.lbJugarPingPong.TabIndex = 2;
+            this.lbJugarPingPong.Text = "Jugar Ping Pong";
+            this.lbJugarPingPong.MouseLeave += new System.EventHandler(this.lbJugarPingPong_MouseLeave);
+            this.lbJugarPingPong.MouseHover += new System.EventHandler(this.lbJugarPingPong_MouseHover);
+            // 
+            // lbCerrarPrograma
+            // 
+            this.lbCerrarPrograma.AutoSize = true;
+            this.lbCerrarPrograma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbCerrarPrograma.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbCerrarPrograma.Location = new System.Drawing.Point(231, 347);
+            this.lbCerrarPrograma.Name = "lbCerrarPrograma";
+            this.lbCerrarPrograma.Size = new System.Drawing.Size(126, 20);
+            this.lbCerrarPrograma.TabIndex = 3;
+            this.lbCerrarPrograma.Text = "Cerrar Programa";
+            this.lbCerrarPrograma.MouseLeave += new System.EventHandler(this.lbCerrarPrograma_MouseLeave);
+            this.lbCerrarPrograma.MouseHover += new System.EventHandler(this.lbCerrarPrograma_MouseHover);
+            // 
+            // pbSelect1
+            // 
+            this.pbSelect1.Image = ((System.Drawing.Image)(resources.GetObject("pbSelect1.Image")));
+            this.pbSelect1.Location = new System.Drawing.Point(136, 187);
+            this.pbSelect1.Name = "pbSelect1";
+            this.pbSelect1.Size = new System.Drawing.Size(58, 63);
+            this.pbSelect1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSelect1.TabIndex = 4;
+            this.pbSelect1.TabStop = false;
+            this.pbSelect1.Visible = false;
+            this.pbSelect1.Click += new System.EventHandler(this.pbSelect1_Click);
+            // 
+            // pbSelect2
+            // 
+            this.pbSelect2.Image = ((System.Drawing.Image)(resources.GetObject("pbSelect2.Image")));
+            this.pbSelect2.Location = new System.Drawing.Point(166, 257);
+            this.pbSelect2.Name = "pbSelect2";
+            this.pbSelect2.Size = new System.Drawing.Size(58, 63);
+            this.pbSelect2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSelect2.TabIndex = 5;
+            this.pbSelect2.TabStop = false;
+            this.pbSelect2.Visible = false;
+            this.pbSelect2.Click += new System.EventHandler(this.pbSelect2_Click);
+            // 
+            // pbSelect3
+            // 
+            this.pbSelect3.Image = ((System.Drawing.Image)(resources.GetObject("pbSelect3.Image")));
+            this.pbSelect3.Location = new System.Drawing.Point(167, 326);
+            this.pbSelect3.Name = "pbSelect3";
+            this.pbSelect3.Size = new System.Drawing.Size(58, 63);
+            this.pbSelect3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSelect3.TabIndex = 6;
+            this.pbSelect3.TabStop = false;
+            this.pbSelect3.Visible = false;
+            this.pbSelect3.Click += new System.EventHandler(this.pbSelect3_Click);
+            // 
+            // lbAyuda
+            // 
+            this.lbAyuda.AutoSize = true;
+            this.lbAyuda.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.lbAyuda.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbAyuda.Location = new System.Drawing.Point(279, 415);
+            this.lbAyuda.Name = "lbAyuda";
+            this.lbAyuda.Size = new System.Drawing.Size(54, 20);
+            this.lbAyuda.TabIndex = 7;
+            this.lbAyuda.Text = "Ayuda";
+            this.lbAyuda.MouseLeave += new System.EventHandler(this.lbAyuda_MouseLeave);
+            this.lbAyuda.MouseHover += new System.EventHandler(this.lbAyuda_MouseHover);
+            // 
+            // pbSelect4
+            // 
+            this.pbSelect4.Image = ((System.Drawing.Image)(resources.GetObject("pbSelect4.Image")));
+            this.pbSelect4.Location = new System.Drawing.Point(215, 395);
+            this.pbSelect4.Name = "pbSelect4";
+            this.pbSelect4.Size = new System.Drawing.Size(58, 63);
+            this.pbSelect4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbSelect4.TabIndex = 8;
+            this.pbSelect4.TabStop = false;
+            this.pbSelect4.Visible = false;
+            this.pbSelect4.Click += new System.EventHandler(this.pbSelect4_Click);
+            // 
+            // lblStart
+            // 
+            this.lblStart = new System.Windows.Forms.Label();
+            this.lblStart.AutoSize = false;
+            this.lblStart.Name = "lblStart";
+            this.lblStart.Text = "PRESS START";
+            this.lblStart.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblStart.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblStart.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblStart.Location = new System.Drawing.Point(100, 200);
+            this.lblStart.Size = new System.Drawing.Size(437, 60);
+            this.lblStart.DoubleClick += new System.EventHandler(this.lblStart_DoubleClick);
+            this.Controls.Add(this.lblStart);
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(637, 518);
+            this.Controls.Add(this.pbSelect4);
+            this.Controls.Add(this.lbAyuda);
+            this.Controls.Add(this.pbSelect3);
+            this.Controls.Add(this.pbSelect2);
+            this.Controls.Add(this.pbSelect1);
+            this.Controls.Add(this.lbCerrarPrograma);
+            this.Controls.Add(this.lbJugarPingPong);
+            this.Controls.Add(this.lbGeneracion);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu principal";
+            this.Load += new System.EventHandler(this.MenuPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbSelect4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label lbGeneracion;
         private System.Windows.Forms.Label lbJugarPingPong;
@@ -195,6 +371,10 @@
         private System.Windows.Forms.PictureBox pbSelect3;
         private System.Windows.Forms.Label lbAyuda;
         private System.Windows.Forms.PictureBox pbSelect4;
+        private System.Windows.Forms.Label lblStart;
     }
 }
+
+
+    
 
--- Formularios/Menu principal.cs
+++ Formularios/Menu principal.cs
@@ -7,16 +7,102 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MatrizLED;
 
 namespace PingPong_Generacion_de_figuras_Grupo3
 {
     public partial class MenuPrincipal : Form
     {
+        int opcionselecionada = 0;
+        Label[] opcioneslabels;
+        PictureBox[] opcionesIndicadores;
+
         public MenuPrincipal()
         {
             InitializeComponent();
+            this.KeyPreview = true;
         }
 
+        private void MenuPrincipal_Load(object sender, EventArgs e)
+        {
+            opcioneslabels = new Label[]
+            {
+                lbGeneracion, lbJugarPingPong, lbCerrarPrograma, lbAyuda
+            };
+
+            opcionesIndicadores = new PictureBox[]
+            {
+                pbSelect1, pbSelect2, pbSelect3, pbSelect4
+            };
+
+            // Oculta todo menos lbStart
+            foreach (var label in opcioneslabels) label.Visible = false;
+            foreach (var pb in opcionesIndicadores) pb.Visible = false;
+            lblStart.Visible = true;
+        }
+
+        private void MenuPrincipal_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (lblStart.Visible && e.KeyCode == Keys.Enter)
+            {
+                lblStart.Visible = false;
+
+                foreach (var label in opcioneslabels) label.Visible = true;
+                foreach (var pb in opcionesIndicadores) pb.Visible = false;
+
+                opcionesIndicadores[opcionselecionada].Visible = true;
+            }
+            else if (!lblStart.Visible)
+            {
+                if (e.KeyCode == Keys.Up)
+                {
+                    int nueva = (opcionselecionada - 1 + opcioneslabels.Length) % opcioneslabels.Length;
+                    CambiarSeleccion(nueva);
+                }
+                else if (e.KeyCode == Keys.Down)
+                {
+                    int nueva = (opcionselecionada + 1) % opcioneslabels.Length;
+                    CambiarSeleccion(nueva);
+                }
+                else if (e.KeyCode == Keys.Enter)
+                {
+                    EjecutarOpcion(opcionselecionada);
+                }
+            }
+        }
+
+        private void CambiarSeleccion(int nuevaSeleccion)
+        {
+            if (nuevaSeleccion < 0 || nuevaSeleccion >= opcioneslabels.Length)
+                return;
+
+            opcionesIndicadores[opcionselecionada].Visible = false;
+            opcionselecionada = nuevaSeleccion;
+            opcionesIndicadores[opcionselecionada].Visible = true;
+        }
+
+        private void EjecutarOpcion(int index)
+        {
+            switch (index)
+            {
+                case 0:
+                    new CrearNuevaFigura().Show();
+                    this.Hide();
+                    break;
+                case 1:
+                    // new JuegoPingPong().Show();
+                    this.Hide();
+                    break;
+                case 2:
+                    Application.Exit();
+                    break;
+                case 3:
+                    MessageBox.Show("Ayuda: Usa las flechas ↑ ↓ para moverte y Enter para seleccionar.", "Ayuda");
+                    break;
+            }
+        }
+
+        // ==== Eventos MouseHover y MouseLeave (estilo clásico) ====
         private void lbGeneracion_MouseHover(object sender, EventArgs e)
         {
             pbSelect1.Visible = true;
@@ -57,9 +143,37 @@ namespace PingPong_Generacion_de_figuras_Grupo3
             pbSelect4.Visible = false;
         }
 
+        // ==== Clicks en los PictureBox ====
         private void pbSelect1_Click(object sender, EventArgs e)
         {
-
+            EjecutarOpcion(0);
         }
+
+        private void pbSelect2_Click(object sender, EventArgs e)
+        {
+            EjecutarOpcion(1);
+        }
+
+        private void pbSelect3_Click(object sender, EventArgs e)
+        {
+            EjecutarOpcion(2);
+        }
+
+        private void pbSelect4_Click(object sender, EventArgs e)
+        {
+            EjecutarOpcion(3);
+        }
+        private void lblStart_DoubleClick(object sender, EventArgs e)
+        {
+            // Aquí defines qué debe ocurrir cuando se haga doble clic
+            lblStart.Visible = false;
+
+            // Mostrar las opciones del menú
+            foreach (var label in opcioneslabels) label.Visible = true;
+            opcionesIndicadores[opcionselecionada].Visible = true;
+        }
+
     }
+
 }
+
