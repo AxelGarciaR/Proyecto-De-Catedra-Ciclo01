0fee610e3c9c49cc4a4a863c3172d4443af77545
AxelGarciaR <114636756+AxelGarciaR@users.noreply.github.com>
2025-05-07 21:58:00 -0600

    prueba de formularios faltantes

---
+++ Formularios/PingPong tablero.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace PingPong_Generacion_de_figuras_Grupo3.Formularios
+{
+    partial class PingPongGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PingPongGame));
+            this.timerPingPong = new System.Windows.Forms.Timer(this.components);
+            this.pbxJugador1 = new System.Windows.Forms.PictureBox();
+            this.pbxJugador2 = new System.Windows.Forms.PictureBox();
+            this.pbxPelota = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.lbPuntaje2 = new System.Windows.Forms.Label();
+            this.lbPuntaje1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxJugador1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxJugador2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPelota)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerPingPong
+            // 
+            this.timerPingPong.Tick += new System.EventHandler(this.timerPingPong_Tick);
+            // 
+            // pbxJugador1
+            // 
+            this.pbxJugador1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.pbxJugador1.Location = new System.Drawing.Point(12, 150);
+            this.pbxJugador1.Name = "pbxJugador1";
+            this.pbxJugador1.Size = new System.Drawing.Size(20, 100);
+            this.pbxJugador1.TabIndex = 0;
+            this.pbxJugador1.TabStop = false;
+            // 
+            // pbxJugador2
+            // 
+            this.pbxJugador2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.pbxJugador2.Location = new System.Drawing.Point(618, 150);
+            this.pbxJugador2.Name = "pbxJugador2";
+            this.pbxJugador2.Size = new System.Drawing.Size(20, 100);
+            this.pbxJugador2.TabIndex = 1;
+            this.pbxJugador2.TabStop = false;
+            this.pbxJugador2.Click += new System.EventHandler(this.pbxJugador2_Click);
+            // 
+            // pbxPelota
+            // 
+            this.pbxPelota.BackColor = System.Drawing.Color.Transparent;
+            this.pbxPelota.Image = ((System.Drawing.Image)(resources.GetObject("pbxPelota.Image")));
+            this.pbxPelota.Location = new System.Drawing.Point(300, 176);
+            this.pbxPelota.Name = "pbxPelota";
+            this.pbxPelota.Size = new System.Drawing.Size(50, 50);
+            this.pbxPelota.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxPelota.TabIndex = 2;
+            this.pbxPelota.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox1.Location = new System.Drawing.Point(320, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(10, 50);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox2.Location = new System.Drawing.Point(320, 102);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(10, 50);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox4.Location = new System.Drawing.Point(320, 288);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(10, 50);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox5.Location = new System.Drawing.Point(320, 388);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(10, 50);
+            this.pictureBox5.TabIndex = 8;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pictureBox6.Location = new System.Drawing.Point(320, 200);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(10, 50);
+            this.pictureBox6.TabIndex = 9;
+            this.pictureBox6.TabStop = false;
+            // 
+            // lbPuntaje2
+            // 
+            this.lbPuntaje2.AutoSize = true;
+            this.lbPuntaje2.BackColor = System.Drawing.Color.Transparent;
+            this.lbPuntaje2.Font = new System.Drawing.Font("Monocraft", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPuntaje2.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbPuntaje2.Location = new System.Drawing.Point(389, 12);
+            this.lbPuntaje2.Name = "lbPuntaje2";
+            this.lbPuntaje2.Size = new System.Drawing.Size(40, 43);
+            this.lbPuntaje2.TabIndex = 11;
+            this.lbPuntaje2.Text = "0";
+            // 
+            // lbPuntaje1
+            // 
+            this.lbPuntaje1.AutoSize = true;
+            this.lbPuntaje1.BackColor = System.Drawing.Color.Transparent;
+            this.lbPuntaje1.Font = new System.Drawing.Font("Monocraft", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbPuntaje1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lbPuntaje1.Location = new System.Drawing.Point(226, 12);
+            this.lbPuntaje1.Name = "lbPuntaje1";
+            this.lbPuntaje1.Size = new System.Drawing.Size(40, 43);
+            this.lbPuntaje1.TabIndex = 12;
+            this.lbPuntaje1.Text = "0";
+            // 
+            // PingPongGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(650, 450);
+            this.Controls.Add(this.lbPuntaje1);
+            this.Controls.Add(this.lbPuntaje2);
+            this.Controls.Add(this.pbxPelota);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pbxJugador2);
+            this.Controls.Add(this.pbxJugador1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PingPongGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PingPongGame";
+            this.Load += new System.EventHandler(this.PingPongGame_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PingPongGame_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxJugador1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxJugador2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxPelota)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerPingPong;
+        private System.Windows.Forms.PictureBox pbxJugador1;
+        private System.Windows.Forms.PictureBox pbxJugador2;
+        private System.Windows.Forms.PictureBox pbxPelota;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label lbPuntaje2;
+        private System.Windows.Forms.Label lbPuntaje1;
+    }
+}
---
+++ Formularios/PingPongMenuP.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace PingPong_Generacion_de_figuras_Grupo3
+{
+    partial class PingPongMenuP
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PingPongMenuP));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbIniciarPingPong = new System.Windows.Forms.Label();
+            this.lbSalirMenu = new System.Windows.Forms.Label();
+            this.lbSalirAplicacion = new System.Windows.Forms.Label();
+            this.PbSalir = new System.Windows.Forms.PictureBox();
+            this.PbMenu = new System.Windows.Forms.PictureBox();
+            this.PbRean = new System.Windows.Forms.PictureBox();
+            this.lbControles = new System.Windows.Forms.Label();
+            this.PbReini = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PbSalir)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbMenu)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbRean)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbReini)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Monocraft", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(79, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(565, 85);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¡BIENVENIDO!";
+            // 
+            // lbIniciarPingPong
+            // 
+            this.lbIniciarPingPong.AutoSize = true;
+            this.lbIniciarPingPong.Font = new System.Drawing.Font("Monocraft", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIniciarPingPong.ForeColor = System.Drawing.Color.White;
+            this.lbIniciarPingPong.Location = new System.Drawing.Point(284, 202);
+            this.lbIniciarPingPong.Name = "lbIniciarPingPong";
+            this.lbIniciarPingPong.Size = new System.Drawing.Size(117, 28);
+            this.lbIniciarPingPong.TabIndex = 1;
+            this.lbIniciarPingPong.Text = "Iniciar";
+            this.lbIniciarPingPong.Click += new System.EventHandler(this.lbIniciarPingPong_Click);
+            this.lbIniciarPingPong.MouseLeave += new System.EventHandler(this.lbReanudar_MouseLeave);
+            this.lbIniciarPingPong.MouseHover += new System.EventHandler(this.lbReanudar_MouseHover);
+            // 
+            // lbSalirMenu
+            // 
+            this.lbSalirMenu.AutoSize = true;
+            this.lbSalirMenu.Font = new System.Drawing.Font("Monocraft", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSalirMenu.ForeColor = System.Drawing.Color.White;
+            this.lbSalirMenu.Location = new System.Drawing.Point(284, 403);
+            this.lbSalirMenu.Name = "lbSalirMenu";
+            this.lbSalirMenu.Size = new System.Drawing.Size(207, 28);
+            this.lbSalirMenu.TabIndex = 3;
+            this.lbSalirMenu.Text = "Salir al menu";
+            this.lbSalirMenu.MouseLeave += new System.EventHandler(this.LbSalirAlmenu_MouseLeave);
+            this.lbSalirMenu.MouseHover += new System.EventHandler(this.LbSalirAlmenu_MouseHover);
+            // 
+            // lbSalirAplicacion
+            // 
+            this.lbSalirAplicacion.AutoSize = true;
+            this.lbSalirAplicacion.Font = new System.Drawing.Font("Monocraft", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSalirAplicacion.ForeColor = System.Drawing.Color.White;
+            this.lbSalirAplicacion.Location = new System.Drawing.Point(284, 336);
+            this.lbSalirAplicacion.Name = "lbSalirAplicacion";
+            this.lbSalirAplicacion.Size = new System.Drawing.Size(87, 28);
+            this.lbSalirAplicacion.TabIndex = 4;
+            this.lbSalirAplicacion.Text = "Salir";
+            this.lbSalirAplicacion.Click += new System.EventHandler(this.lbSalirAplicacion_Click);
+            this.lbSalirAplicacion.MouseLeave += new System.EventHandler(this.LbSalir_MouseLeave);
+            this.lbSalirAplicacion.MouseHover += new System.EventHandler(this.LbSalir_MouseHover);
+            // 
+            // PbSalir
+            // 
+            this.PbSalir.Image = ((System.Drawing.Image)(resources.GetObject("PbSalir.Image")));
+            this.PbSalir.Location = new System.Drawing.Point(228, 323);
+            this.PbSalir.Name = "PbSalir";
+            this.PbSalir.Size = new System.Drawing.Size(50, 53);
+            this.PbSalir.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbSalir.TabIndex = 7;
+            this.PbSalir.TabStop = false;
+            this.PbSalir.Visible = false;
+            // 
+            // PbMenu
+            // 
+            this.PbMenu.Image = ((System.Drawing.Image)(resources.GetObject("PbMenu.Image")));
+            this.PbMenu.Location = new System.Drawing.Point(228, 392);
+            this.PbMenu.Name = "PbMenu";
+            this.PbMenu.Size = new System.Drawing.Size(50, 53);
+            this.PbMenu.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbMenu.TabIndex = 8;
+            this.PbMenu.TabStop = false;
+            this.PbMenu.Visible = false;
+            // 
+            // PbRean
+            // 
+            this.PbRean.Image = ((System.Drawing.Image)(resources.GetObject("PbRean.Image")));
+            this.PbRean.Location = new System.Drawing.Point(228, 190);
+            this.PbRean.Name = "PbRean";
+            this.PbRean.Size = new System.Drawing.Size(50, 53);
+            this.PbRean.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbRean.TabIndex = 10;
+            this.PbRean.TabStop = false;
+            this.PbRean.Visible = false;
+            // 
+            // lbControles
+            // 
+            this.lbControles.AutoSize = true;
+            this.lbControles.Font = new System.Drawing.Font("Monocraft", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbControles.ForeColor = System.Drawing.Color.White;
+            this.lbControles.Location = new System.Drawing.Point(284, 268);
+            this.lbControles.Name = "lbControles";
+            this.lbControles.Size = new System.Drawing.Size(147, 28);
+            this.lbControles.TabIndex = 2;
+            this.lbControles.Text = "Controles";
+            this.lbControles.MouseLeave += new System.EventHandler(this.LbReiniciar_MouseLeave);
+            this.lbControles.MouseHover += new System.EventHandler(this.LbReiniciar_MouseHover);
+            // 
+            // PbReini
+            // 
+            this.PbReini.Image = ((System.Drawing.Image)(resources.GetObject("PbReini.Image")));
+            this.PbReini.Location = new System.Drawing.Point(228, 256);
+            this.PbReini.Name = "PbReini";
+            this.PbReini.Size = new System.Drawing.Size(50, 53);
+            this.PbReini.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbReini.TabIndex = 9;
+            this.PbReini.TabStop = false;
+            this.PbReini.Visible = false;
+            // 
+            // PingPongMenuP
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(708, 534);
+            this.Controls.Add(this.PbRean);
+            this.Controls.Add(this.PbReini);
+            this.Controls.Add(this.PbMenu);
+            this.Controls.Add(this.PbSalir);
+            this.Controls.Add(this.lbSalirAplicacion);
+            this.Controls.Add(this.lbSalirMenu);
+            this.Controls.Add(this.lbControles);
+            this.Controls.Add(this.lbIniciarPingPong);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "PingPongMenuP";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PingPong";
+            this.Load += new System.EventHandler(this.PingPongMenuP_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.PbSalir)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbMenu)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbRean)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.PbReini)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbIniciarPingPong;
+        private System.Windows.Forms.Label lbSalirMenu;
+        private System.Windows.Forms.Label lbSalirAplicacion;
+        private System.Windows.Forms.PictureBox PbSalir;
+        private System.Windows.Forms.PictureBox PbMenu;
+        private System.Windows.Forms.PictureBox PbRean;
+        private System.Windows.Forms.Label lbControles;
+        private System.Windows.Forms.PictureBox PbReini;
+    }
+}
