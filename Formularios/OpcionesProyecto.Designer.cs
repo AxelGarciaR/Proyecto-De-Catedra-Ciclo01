2a8e818f65546858a0137cb96df5e65fc603c6ab
AxelGarciaR <114636756+AxelGarciaR@users.noreply.github.com>
2025-05-20 14:28:20 -0600

    Formulario de Opciones proyecto

---
+++ Formularios/OpcionesProyecto.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace PingPong_Generacion_de_figuras_Grupo3.Formularios
+{
+    partial class OpcionesProyecto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbMatriz = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tckbarBrillo = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnRegresar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.tckbarBrillo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbMatriz
+            // 
+            this.lbMatriz.AutoSize = true;
+            this.lbMatriz.Font = new System.Drawing.Font("Monocraft", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMatriz.ForeColor = System.Drawing.Color.White;
+            this.lbMatriz.Location = new System.Drawing.Point(100, 27);
+            this.lbMatriz.Name = "lbMatriz";
+            this.lbMatriz.Size = new System.Drawing.Size(237, 28);
+            this.lbMatriz.TabIndex = 2;
+            this.lbMatriz.Text = "Opciones Matriz";
+            this.lbMatriz.Click += new System.EventHandler(this.lbMatriz_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.label1.Location = new System.Drawing.Point(42, 178);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(351, 20);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "______________________________________";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tckbarBrillo
+            // 
+            this.tckbarBrillo.LargeChange = 1;
+            this.tckbarBrillo.Location = new System.Drawing.Point(46, 130);
+            this.tckbarBrillo.Maximum = 15;
+            this.tckbarBrillo.Name = "tckbarBrillo";
+            this.tckbarBrillo.Size = new System.Drawing.Size(347, 45);
+            this.tckbarBrillo.TabIndex = 27;
+            this.tckbarBrillo.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.tckbarBrillo.Scroll += new System.EventHandler(this.tckbarBrillo_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Monocraft", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(43, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 16);
+            this.label2.TabIndex = 28;
+            this.label2.Text = "Cambiar intensidad";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Monocraft", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(43, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 29;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnGuardar.Location = new System.Drawing.Point(47, 213);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(164, 38);
+            this.btnGuardar.TabIndex = 31;
+            this.btnGuardar.Text = "Guardar cambios";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // btnRegresar
+            // 
+            this.btnRegresar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(32)))), ((int)(((byte)(87)))));
+            this.btnRegresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegresar.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRegresar.Location = new System.Drawing.Point(224, 213);
+            this.btnRegresar.Name = "btnRegresar";
+            this.btnRegresar.Size = new System.Drawing.Size(163, 38);
+            this.btnRegresar.TabIndex = 32;
+            this.btnRegresar.Text = "Regresar";
+            this.btnRegresar.UseVisualStyleBackColor = false;
+            this.btnRegresar.Click += new System.EventHandler(this.btnRegresar_Click);
+            // 
+            // OpcionesProyecto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(56)))));
+            this.ClientSize = new System.Drawing.Size(447, 281);
+            this.Controls.Add(this.btnRegresar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tckbarBrillo);
+            this.Controls.Add(this.lbMatriz);
+            this.Name = "OpcionesProyecto";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Opciones matriz";
+            this.Load += new System.EventHandler(this.OpcionesProyecto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tckbarBrillo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbMatriz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar tckbarBrillo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnRegresar;
+    }
+}
